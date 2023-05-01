87b3d492de3d89658be388d9e31e1f6ad5037d4e
nazarevaa <L120702@mail.ru>
2023-05-01 21:23:06 +0600

    In

--- src/mainC.java
+++ src/mainC.java
@@ -1,5 +1,23 @@
-public class Main {
+public class mainC {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ArrList<Integer> IntArrList = new ArrList<>();
+        IntArrList.add(12);
+        IntArrList.add(34);
+        IntArrList.add(90);
+        IntArrList.add(789);
+        IntArrList.add(56);
+        System.out.println("0: " + IntArrList.getByIndex(0));
+        System.out.println("1: " + IntArrList.getByIndex(1));
+        System.out.println("2: " + IntArrList.getByIndex(2));
+        System.out.println("4: " + IntArrList.getByIndex(4));
+        System.out.println("3 before deleting :" + IntArrList.getByIndex(3));
+        System.out.println("Delete 3 ...");
+        IntArrList.remove(3);
+        System.out.println("3 after deleting:" + IntArrList.getByIndex(3));
+
+
     }
 }
